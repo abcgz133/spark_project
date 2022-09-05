f80b494d81144ff4291438d2e972c5fe665d2292
abcgz <abcgz@126.com>
2022-09-05 15:21:02 +0800

    v0.1 fiveMinutesTrend

---
+++ BlackList_filter_create/BlackList_filter_create.scala
@@ -0,0 +1,160 @@
+package sparkProject.BlackList_filter_create
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import util.JDBCUtil
+
+import java.sql.{PreparedStatement, ResultSet}
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.collection.mutable.ListBuffer
+
+object BlackList_filter_create {
+  /*
+    1. receive data from Kafka,
+    2. transform the data to DStream and parse the data to a case class
+    3. select user_id from black_list, then filter the data where user_id is in the black_list
+    4. map the filtered data to ((day, user_id, ad_id),1)  and then reduce to count this data
+    the result is the count of ad clicked.
+    5. if count >20 then insert or update the blacklist
+    6. if count <=20, then update or insert into the user_ad_count ,
+    if the updated count >20, then insert or update the user_id into  black_list
+
+     */
+
+  def main(args: Array[String]): Unit = {
+
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+
+    val kafkaPara: Map[String, Object] = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "hadoop01:9092,hadoop01:9092,hadoop01:9092",
+      ConsumerConfig.GROUP_ID_CONFIG -> "aiShengYing",
+      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+    )
+
+    val kafkaDataDS: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](Set("aiShengYing"), kafkaPara)
+    )
+
+    val dataAdDstream: DStream[AdClickData] = kafkaDataDS.map(
+      kafkadata => {
+        val dataArray: Array[String] = kafkadata.value().split(" ")
+        AdClickData(
+          dataArray(0),
+          dataArray(1),
+          dataArray(2),
+          dataArray(3),
+          dataArray(4)
+        )
+      }
+    )
+    //filter the black_list from the data
+    val reducedGroupedDstream: DStream[((String, String, String), Int)] = dataAdDstream.transform(
+      rdd => {
+        val conn = JDBCUtil.getConnection
+        val preparedStatement: PreparedStatement = conn.prepareStatement("select user_id from black_list")
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+        var black_list = ListBuffer[String]()
+        while (resultSet.next()) {
+          black_list.append(resultSet.getString(1))
+        }
+        resultSet.close()
+        preparedStatement.close()
+        conn.close()
+
+        val filteredRDD: RDD[AdClickData] = rdd.filter(
+          data => {
+            !black_list.contains(data.user_id)
+          }
+        )
+
+        filteredRDD.map(
+          data => {
+            val sdf = new SimpleDateFormat("yyyy-MM-dd")
+            val day1: String = sdf.format(new Date(data.day.toLong))
+            ((day1, data.user_id, data.ad_id), 1)
+          }
+        ).reduceByKey(_ + _)
+
+      }
+    )
+
+    //reducedGroupedDstream: DStream[((String, String, String), Int)]
+    reducedGroupedDstream.foreachRDD(
+      rdd => {
+        rdd.foreach {
+
+          case ((day, user_id, ad_id), count) => {
+            println(s"day: ${day} user_id:${user_id} ad_id:${ad_id} count:${count} ")
+            // if count <20, insert or update the user_ad_count
+            // then if the new counted > 20 then insert or update into
+            // the black_list
+            if (count < 20) {
+              val conn = JDBCUtil.getConnection
+              val sql1 =
+                """
+                  |insert into user_ad_count (day, user_id, ad_id, count) values (?,?,?,?)
+                  |on duplicate key
+                  |update count = count + ?
+                  |""".stripMargin
+              JDBCUtil.executeUpdate(conn, sql1, Array(day, user_id, ad_id, count, count))
+
+              val sql2 =
+                """
+                  |select user_id from user_ad_count
+                  |where day = ?
+                  |and user_id = ?
+                  |and ad_id = ?
+                  |and count >= 20
+                  |""".stripMargin
+              val flag: Boolean = JDBCUtil.isExist(conn, sql2, Array(day, user_id, ad_id))
+
+              if (flag) {
+                val sql3 =
+                  """
+                    |insert into black_list (user_id) values (?)
+                    |on duplicate key
+                    |update user_id = ?
+                    |""".stripMargin
+                JDBCUtil.executeUpdate(conn, sql3, Array(user_id, user_id))
+
+              }
+              conn.close()
+            } else {
+              // count >= 20
+              val conn = JDBCUtil.getConnection
+              val sql4 =
+                """
+                  |insert into black_list (user_id) values (?)
+                  |on duplicate key
+                  |update user_id = ?
+                  |""".stripMargin
+              JDBCUtil.executeUpdate(conn, sql4, Array(user_id, user_id))
+              conn.close()
+            }
+
+
+          }
+
+        }
+      }
+    )
+
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  // data of ad clicking
+  case class AdClickData(day: String, area: String, city: String, user_id: String, ad_id: String)
+
+}
---
+++ First2cities_in_3_category_in_eachArea/First2cities_in_3_category_in_eachArea.scala
@@ -0,0 +1,229 @@
+package sparkProject.First2cities_in_3_category_in_eachArea
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.expressions.Aggregator
+import org.apache.spark.sql.{Encoder, Encoders, SparkSession, functions}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object First2cities_in_3_category_in_eachArea {
+  /*
+    1. parse the textFile to table user_action, city_info and product_info
+    2. select all necessary data from a big table(named big_table1) that join these 3 tables.
+    3. select the areas, the products ,the rank , and the city_rate from the big_table1 group by
+    area, product_name to form an another big table(named big_table2)
+    4. to count and order the city_rate by extends the UDAF of Aggregator
+    5. to select * from big_table2 where the rank <=3 to form an another big table(named big_table3)
+
+   Specially, The creation and destruction of connection objects will cost the time heavily.
+Therefore, a connection object such as a connection pool is used for each RDD partition.
+foreachPartition for RDD in DStream is used to create a connection object for each partition in RDD,
+ and use a connection object to write the data in a partition to MySQL. This can greatly reduces
+ the number of connection objects created.
+
+     */
+
+  def main(args: Array[String]): Unit = {
+    val sparConf = new SparkConf().setMaster("local[*]").setAppName("first2cities_in_3_catagories")
+    val sc = new SparkContext(sparConf)
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[2]")
+      .getOrCreate()
+
+    //parse the textFile to table user_action, city_info and product_info
+    val dataRDD: RDD[String] = sc.textFile("datas/user_visit_action.txt")
+    val userActionRDD: RDD[UserVisitAction] = dataRDD.map(
+      line => {
+        val lineArray: Array[String] = line.split("_")
+        UserVisitAction(
+          lineArray(0),
+          lineArray(1).toLong,
+          lineArray(2),
+          lineArray(3).toLong,
+          lineArray(4),
+          lineArray(5),
+          lineArray(6).toLong,
+          lineArray(7).toLong,
+          lineArray(8),
+          lineArray(9),
+          lineArray(10),
+          lineArray(11),
+          lineArray(12).toLong
+        )
+      }
+    )
+
+    userActionRDD.cache()
+    import spark.implicits._
+    userActionRDD.toDF().createOrReplaceTempView("user_visit_action")
+    spark.sql("select * from user_visit_action").show()
+
+    val cityDataRDD: RDD[String] = sc.textFile("datas/city_info.txt")
+    val cityINFORDD: RDD[CityInfo] = cityDataRDD.map(
+      line => {
+        val citylineArray: Array[String] = line.split("\t")
+        CityInfo(
+          citylineArray(0).toLong,
+          citylineArray(1),
+          citylineArray(2)
+        )
+      }
+    )
+    cityDataRDD.cache()
+
+    cityINFORDD.toDF().createOrReplaceTempView("city_info")
+    spark.sql("select * from city_info").show()
+
+    val productDataRDD: RDD[String] = sc.textFile("datas/product_info.txt")
+    val productRDD: RDD[ProductInfo] = productDataRDD.map(
+      line => {
+        val productArray: Array[String] = line.split("\t")
+        ProductInfo(
+          productArray(0).toLong,
+          productArray(1),
+          productArray(2)
+        )
+      }
+    )
+    productRDD.cache()
+    productRDD.toDF().createOrReplaceTempView("product_info")
+    spark.sql("select * from product_info").show()
+
+    //select all necessary data from a big table(named big_table1) that join these 3 tables.
+    spark.sql(
+      """
+        |select *,area, city_name, product_name  from user_visit_action u
+        |join city_info c on c.city_id = u.city_id
+        |join product_info p on u.click_product_id = p.product_id
+        |where u.click_category_id != -1
+        |""".stripMargin).createOrReplaceTempView("big_table1")
+
+    //select the areas, the products ,the rank , and the city_rate from the big_table1 group by
+    // area, product_name to form an another big table(named big_table2)
+    spark.udf.register("cityRate", functions.udaf(new CityRateUDAF()))
+    spark.sql(
+      """
+        |select area, product_name as NAME,
+        |count(*) as CLICK_CNT,
+        |cityRate(city_name) as CITY_RATE
+        |from big_table1
+        |group by area, product_name
+        |""".stripMargin).createOrReplaceTempView("big_table2")
+
+    //to select * from big_table2 where the rank <=3 to form an another big table(named big_table3)
+    spark.sql(
+      """
+        |select *, rank() over(partition by area order by CLICK_CNT desc ) as RANK from big_table2
+        |""".stripMargin).createOrReplaceTempView("big_table3")
+
+    //to select * from big_table2 where the rank <=3 to form an another big table(named big_table3)
+    spark.sql(
+      """
+        |select * from big_table3 where RANK <=3
+        |""".stripMargin).show()
+
+    sc.stop()
+    spark.close()
+  }
+
+  case class UserVisitAction(
+                              date: String, //the date of user click
+                              user_id: Long, //user id
+                              session_id: String, //Session ID
+                              page_id: Long, //PageID
+                              action_time: String, //action time
+                              search_keyword: String, //key word in search
+                              click_category_id: Long, // category ID of selling goods
+                              click_product_id: Long, // product ID
+                              order_category_ids: String, //category ids in one ordering
+                              order_product_ids: String, // product_ids in one ordering
+                              pay_category_ids: String, // category ids in one paying
+                              pay_product_ids: String, // product_ids in one paying
+                              city_id: Long    // city id
+                            )
+
+
+  case class CityInfo(
+                       city_id: Long,
+                       city_name: String,
+                       area: String
+                     )
+
+  case class ProductInfo(
+                          product_id: Long,
+                          product_name: String,
+                          extend_info: String
+                        )
+
+
+  case class Buffer(var total_cnt: Long, var city_map: mutable.Map[String, Long])
+
+  class CityRateUDAF extends Aggregator[String, Buffer, String] {
+    override def zero: Buffer = {
+      Buffer(0L, mutable.Map[String, Long]())
+    }
+
+    override def reduce(buffer: Buffer, city: String): Buffer = {
+      buffer.total_cnt += 1
+      val map: mutable.Map[String, Long] = buffer.city_map
+      val city_new_count: Long = map.getOrElse(city, 0L) + 1
+      map.update(city, city_new_count)
+      buffer.city_map = map
+      buffer
+
+    }
+
+    override def merge(buffer1: Buffer, buffer2: Buffer): Buffer = {
+      buffer1.total_cnt += buffer2.total_cnt
+      val map1: mutable.Map[String, Long] = buffer1.city_map
+      val map2: mutable.Map[String, Long] = buffer2.city_map
+      map2.foreach {
+        case (city, cnt) => {
+          val newCnt: Long = map1.getOrElse(city, 0L) + cnt
+          map1.update(city, newCnt)
+        }
+      }
+      buffer1.city_map = map1
+      buffer1
+    }
+
+    override def finish(reduction: Buffer): String = {
+      /*
+      1. sort the map in reduction ordering with the count of city
+      2. count the rate of each city by dividing by the total_cnt
+      3. take just 2 biggest cities, the rest will be (100%-the 2 biggest cities)
+      4. new a ListBuffer to be appended and at last to be formed to a String
+      */
+      var stringList = ListBuffer[String]()
+      val map: mutable.Map[String, Long] = reduction.city_map
+      val sortedCityList: List[(String, Long)] = map.toList.sortWith(
+        (left, right) => {
+          left._2 > right._2
+        }
+      ).take(2)
+
+      //count the rate of each city by dividing by the total_cnt
+      var sum_of_two = 0L
+      sortedCityList.foreach {
+        case (city, cnt) => {
+          val rate = cnt * 100 / reduction.total_cnt
+          stringList.append(s"${city} ${rate}%")
+          sum_of_two += rate
+        }
+      }
+
+      //the rest will be (100%-the 2 biggest cities)
+      if (map.size > 2) {
+        stringList.append(s"3rd ${100 - sum_of_two}%")
+      }
+      stringList.mkString(",")
+
+    }
+
+    override def bufferEncoder: Encoder[Buffer] = Encoders.product
+
+    override def outputEncoder: Encoder[String] = Encoders.STRING
+  }
+}
